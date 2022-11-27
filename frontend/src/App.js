0e97246b8c770019d497756d4b8499c28f2873f4
Vaishnavi <114218591+vaishdwivedi1@users.noreply.github.com>
2022-11-27 08:18:41 +0530

    shop

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <h1>The balcony life</h1>
+      {/* <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter> */}
+    </div>
+  );
+}
+
+export default App;
